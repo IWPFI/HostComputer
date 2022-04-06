e987bde8a0f4fba502a15de827ddbdd407100670
lWPFl <3141629907@qq.com>
2022-04-06 19:52:50 +0800

    登录界面布局

--- HostComputer/App.xaml.cs
+++ HostComputer/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HostComputer.Views;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +14,14 @@ namespace HostComputer
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+
+            if (new LoginWindow().ShowDialog() == true)
+            {
+                new MainWindow().ShowDialog();
+            }
+            Application.Current.Shutdown();
+        }
     }
 }
